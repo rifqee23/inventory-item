26b9854ff316b0fa37e00afbeb0ca7040f2273fa
rifqee23 <rifqifebrianto746@gmail.com>
2024-11-01 00:13:16 +0700

    chore: implement boostrap component

--- src/components/admin/item/Itemcard.vue
+++ src/components/admin/item/Itemcard.vue
@@ -9,103 +9,37 @@ defineProps({
 </script>
 
 <template>
-  <div class="item-card">
-    <h3>{{ item.nama }}</h3>
-    <p>{{ item.deskripsi }}</p>
-    <p>Stok: {{ item.stok }}</p>
-    <div class="buttons">
-      <button class="edit" @click="$emit('edit-item', item)">Edit</button>
-      <button class="edit" @click="$emit('delete-item', item.kode)">
-        Delete
-      </button>
+  <div class="card">
+    <div>
+      <h3 class="card-title">{{ item.nama }}</h3>
+      <div class="card-body">
+        <p class="card-text">{{ item.deskripsi }}</p>
+        <p class="card-text">Stok: {{ item.stok }}</p>
+        <div class="d-flex gap-2">
+          <button
+            class="btn btn-primary custom-btn"
+            @click="$emit('edit-item', item)"
+          >
+            Edit
+          </button>
+          <button
+            class="btn btn-danger custom-btn"
+            @click="$emit('delete-item', item.kode)"
+          >
+            Delete
+          </button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-.item-card {
-  border: 1px solid #ddd;
-
-  padding: 16px;
-
-  margin-bottom: 10px;
-
-  border-radius: 8px;
-
-  background-color: #f9f9f9;
-
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-
-  transition: transform 0.2s, box-shadow 0.2s;
+.custom-btn {
+  transition: transform 0.3s ease;
 }
 
-.item-card:hover {
-  transform: translateY(-5px);
-
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-}
-
-.item-card h3 {
-  margin: 0 0 10px;
-
-  font-size: 1.5em;
-
-  color: #333;
-}
-
-.item-card p {
-  margin: 5px 0;
-
-  color: #555;
-}
-
-.item-card .stock {
-  font-weight: bold;
-
-  color: #2c3e50;
-}
-
-.item-card .buttons {
-  display: flex;
-
-  gap: 10px;
-
-  margin-top: 10px;
-}
-
-.item-card button {
-  padding: 10px 15px;
-
-  border: none;
-
-  border-radius: 4px;
-
-  cursor: pointer;
-
-  transition: background-color 0.2s;
-}
-
-.item-card button:hover {
-  background-color: #ddd;
-}
-
-.item-card button.edit {
-  background-color: #4caf50;
-
-  color: white;
-}
-
-.item-card button.edit:hover {
-  background-color: #45a049;
-}
-
-.item-card button.delete {
-  background-color: #e74c3c;
-
-  color: white;
-}
-
-.item-card button.delete:hover {
-  background-color: #c0392b;
+.custom-btn:hover {
+  transform: scale(1.1);
 }
 </style>
